8509bc7116aae18f8cc8ee3ae5e96444b840af41
danphenderson <danphenderson@gmail.com>
2023-07-27 13:21:00 -0700

    Updated the theme palette from the theme provider (reverting back to previous revision).

--- frontend/src/ThemeProvider.tsx
+++ frontend/src/ThemeProvider.tsx
@@ -3,8 +3,46 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import { ReactNode } from 'react';
 
-
 const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+    text: {
+      primary: grey[900],
+      secondary: grey[700],
+    },
+    background: {
+      default: '#eee',
+      paper: '#fff',
+    },
+    divider: grey[300],
+  },
+  spacing: 8,
+  typography: {
+    fontFamily: [
+      '"Hiragino Sans"',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      'Helvetica',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {},
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: 'filled',
+      },
+    },
+  },
 })
 
 // Explicitly define the type of the children prop
