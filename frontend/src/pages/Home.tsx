06eb20155533500bd576705b9723841ca9ee40af
danphenderson <danphenderson@gmail.com>
2023-07-27 14:52:41 -0700

    Updates to the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,21 +5,16 @@ import { Typography } from '@mui/material';
 export default function Home() {
   return (
       <Grid container direction="row" component="main" sx={{ height: '100vh' }} >
-        <Grid
-          item
-          component={Paper}
-          xs={12}
-          sm={12}
-          md={12}
+        <Grid item component="div" xs={12} sm={12} md={12}
           sx={{
             backgroundImage: `url(${require("../images/home.jpg")})`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}>
-            <Box sx={{ my: 8, mx: 4 }}>
+          </Grid>
+          <Grid item component={Container}>
               <Typography variant="h1" marginTop={3}>I'm a Developer, a Photographer, and a Climber.</Typography>
-            </Box>
           </Grid>
       </Grid>
   );
