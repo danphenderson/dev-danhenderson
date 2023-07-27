81a2bb52c372703640815a46d01044996fd998a7
danphenderson <danphenderson@gmail.com>
2023-07-26 17:39:48 -0700

    feat : components will be listed within the main app container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import ProTip from './components/ProTip';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Header />
-        <ProTip />
+      
       <Footer />
     </Container>
   );
