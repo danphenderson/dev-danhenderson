0373255fb4879b4810dd5a5fb8d0afd3fc6baf46
danphenderson <danphenderson@gmail.com>
2023-07-28 22:45:15 -0700

    Added recent ticks and displayed them in table. wip

--- frontend/src/pages/Climbing.tsx
+++ frontend/src/pages/Climbing.tsx
@@ -1,6 +1,27 @@
 import * as React from 'react';
 import {Grid, Paper, Box, Card, Container } from '@mui/material';
 import { Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import data from "../assets/climbing/data.json";
+
+const columns = [
+  { field: 'Date', headerName: 'Date', flex: 1 },
+  { field: 'Route', headerName: 'Route', flex: 1 },
+  { field: 'Rating', headerName: 'Rating', flex: 1 },
+  { field: 'Notes', headerName: 'Notes', flex: 1 },
+  { field: 'URL', headerName: 'URL', flex: 1 },
+  { field: 'Pitches', headerName: 'Pitches', flex: 1 },
+  { field: 'Location', headerName: 'Location', flex: 1 },
+  { field: 'Avg Stars', headerName: 'Avg Stars', flex: 1 },
+  { field: 'Your Stars', headerName: 'Your Stars', flex: 1 },
+  { field: 'Style', headerName: 'Style', flex: 1 },
+  { field: 'Lead Style', headerName: 'Lead Style', flex: 1 },
+  { field: 'Route Type', headerName: 'Route Type', flex: 1 },
+  { field: 'Your Rating', headerName: 'Your Rating', flex: 1 },
+  { field: 'Length', headerName: 'Length', flex: 1 },
+  { field: 'Rating Code', headerName: 'Rating Code', flex: 1 },
+];
+
 
 export default function Climbing() {
   return (
@@ -19,6 +40,9 @@ export default function Climbing() {
           }}>
             <Box sx={{ my: 8, mx: 4 }}>
               <Typography variant="h1" marginTop={3}>Climbing</Typography>
+            </Box>
+            <Box>
+              <DataGrid rows={data} columns={columns} />
             </Box>
           </Grid>
       </Grid>
