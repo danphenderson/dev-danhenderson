000559a2d7b2c8e1f4235cfce52c23fddd67f231
danphenderson <danphenderson@gmail.com>
2023-07-29 18:17:28 -0700

    Avatar has been added to the header.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,45 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import {GitHub, LinkedIn, Mail} from '@mui/icons-material';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Box, Button, Stack } from '@mui/material';
+
 
 const pages = ["Photography", "CV", "Climbing", "Contact"];
+const avatar = "./assets/home.jpg";
+
+
 
 export default function Header() {
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-          <Toolbar>
-            {pages.map((page) => (
+      <Toolbar>
+        <IconButton edge="start"  aria-label="avatar" onClick={handleMenu}>
+          <Avatar src={avatar} sx={{ width: 150, height: 150, margin: "0 auto 20px" }} />
+        </IconButton>
+        <Stack direction="row">
+          {
+            pages.map((page) => (
               <Button
                 key={page}
                 size="large"
@@ -22,9 +49,44 @@ export default function Header() {
               >
                 {page}
               </Button>
-            ))}
+           ))
+          }
+        </Stack>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Mail fontSize="small" />
+            </ListItemIcon>
+            <a href="mailto:youremail@gmail.com">Gmail</a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <LinkedIn fontSize="small" />
+            </ListItemIcon>
+            <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <GitHubIcon fontSize="small" />
+            </ListItemIcon>
+            <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+      {/* <AppBar position="static">
+          <Toolbar>
+           
           </Toolbar>
-      </AppBar>
+      </AppBar> */}
     </Box>
   );
 }
