c45c080c91ad49ed2880bf88434d385ef5d62ef9
danphenderson <danphenderson@gmail.com>
2023-07-29 17:47:01 -0700

    Reduced complexity of header.
    Successfully traversing the dom, apart from photography/* searches.
    I am leaving this along for now, as future work is going to remove
    problematic routes and display images albums in a modal.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,70 +2,20 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 
 const pages = ["Photography", "CV", "Climbing", "Contact"];
 
 export default function Header() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
           <Toolbar>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu} href={page.toLowerCase()}>
-                  <Typography noWrap component="div" textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
             {pages.map((page) => (
               <Button
                 key={page}
                 size="large"
-                onClick={handleCloseNavMenu}
                 sx={{color: 'white', display: 'block' }}
                 href={page.toLowerCase()}
                 aria-label={'Go to ' + page}
