ad0c9ac07b43362b04e80821acfac65413fe76db
danphenderson <danphenderson@gmail.com>
2023-08-01 18:05:49 -0700

    Photography.tsx updated with BackgroundPaper

--- frontend/src/pages/Photography.tsx
+++ frontend/src/pages/Photography.tsx
@@ -7,7 +7,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import data from "../photography.json";
-
+import BackgroundPaper from "../components/BackgroundPaper";
 
 
 export default function Photography() {
@@ -19,34 +19,30 @@ export default function Photography() {
   };
 
   return (
-    <Grid container component="main">
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid item component={Container} sx={{ py: 8 }} maxWidth="md">
-            <Grid container spacing={4}>
-              {data.map((card) => (
-                <Grid item key={card.name} xs={12} sm={6} md={4}>
-                  <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                    <CardMedia component="div"
-                      sx={{pt: '80%'}}
-                      image={`${card.src}`}
-                    />
-                    <CardContent sx={{ flexGrow: 1 }}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {card.name}
-                      </Typography>
-                      <Typography>
-                        {card.description}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button href={"photography/" + card.name.toLowerCase()} variant="outlined" onClick={handleAlbumView}>View</Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
+    <BackgroundPaper image='assets/photography/landscape/landscape-lime-kiln.jpg'>
+      <Grid container spacing={4}>
+        {data.map((card) => (
+          <Grid item key={card.name} xs={12} sm={6} md={4}>
+            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+              <CardMedia component="div"
+                sx={{pt: '80%'}}
+                image={`${card.src}`}
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {card.name}
+                </Typography>
+                <Typography>
+                  {card.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button href={"photography/" + card.name.toLowerCase()} variant="outlined" onClick={handleAlbumView}>View</Button>
+              </CardActions>
+            </Card>
           </Grid>
-        </Box>
-    </Grid>
+        ))}
+      </Grid>
+    </BackgroundPaper>
   );
 }
