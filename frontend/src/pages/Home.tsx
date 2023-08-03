0aca6d27aafca8c857f62f58b8abd25950c9ed36
danphenderson <danphenderson@gmail.com>
2023-08-03 14:52:17 -0700

    Using myself for the websites home image.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 import BackgroundPaper from '../components/BackgroundPaper';
 export default function Home() {
   return (
-  <BackgroundPaper image='assets/photography/landscape/landscape-lime-kiln.jpg'>
+  <BackgroundPaper image='assets/home.jpg'>
       <Typography variant="h1" align="center" sx={{ color: '#fff', fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' }, lineHeight: '1.5' }}>
         Hi, I'm a Developer, Photographer, and Climber.
       </Typography>
