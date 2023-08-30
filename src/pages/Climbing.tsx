f3f32ce0cc2798bb40206eb1971402cc082fdb77
danphenderson <danphenderson@gmail.com>
2023-08-29 19:35:41 -0700

    Removing the weather widget since I cannot expose make the request in the frontend, it would expose my token

--- src/pages/Climbing.tsx
+++ src/pages/Climbing.tsx
@@ -1,11 +1,9 @@
-import * as React from 'react';
-import {Grid, Paper, Box, Card, Container, CardActionArea, CardContent, CardMedia, CardActions, Button } from '@mui/material';
+import { Paper} from '@mui/material';
 import { Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import data from "../ticks.json";
 import BackgroundPaper from '../components/BackgroundPaper';
 
-import WeatherWidget from '../components/WeatherWidget';
 
 const columns = [
   { field: 'Date', headerName: 'Date', flex: 1 },
@@ -15,22 +13,12 @@ const columns = [
   { field: 'Pitches', headerName: 'Pitches', flex: 1 },
   { field: 'Location', headerName: 'Location', flex: 1 },
 ];
+
+
 export default function Climbing() {
   return (
     <BackgroundPaper image='assets/photography/landscape/landscape-lime-kiln.jpg'>
       
-      {/* Left Section (Profile Section) */}
-      <Paper elevation={3} sx={{ padding: 2, marginY: 4, width: '70%', minWidth: '600px' }}> {/* Adjusted the width here */}
-        <Box>
-          <Box sx={{ my: 2 }}>
-            <WeatherWidget city="Index" />
-          </Box>
-          <Box sx={{ my: 2 }}>
-            <WeatherWidget city="Seattle" />
-          </Box>
-        </Box>
-      </Paper>
-
       {/* Left Section (Profile Section) TODO: Update ME */}
       <Paper elevation={3} sx={{ padding: 2, marginY: 4 }}>
         {/* Name and Title */}
